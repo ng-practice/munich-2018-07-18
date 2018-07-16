ffe8f5190bfbc45afa689da591e2ac788f5ddb08
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-07-16 15:54:40 +0200

    render list of books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,30 @@ import { Book } from './book/models/book';
 })
 export class AppComponent {
   title = 'app';
-  myFavoriteBook = new Book(
-    '120398235',
-    'Hello Munich',
-    'stellenanzeigen.de',
-    'https://',
-    'Lorem Ipsum',
-    ['Dr. Author', 'Someone Else']
-  );
+  books = [
+    new Book(
+      '120398235',
+      'Hello Munich',
+      'stellenanzeigen.de',
+      'https://',
+      'Lorem Ipsum',
+      ['Dr. Author', 'Someone Else']
+    ),
+    new Book(
+      '12039523235',
+      'Hello Karlsruhe',
+      'stellenanzeigen.de',
+      'https://',
+      'Lorem Ipsum',
+      ['Dr. Author', 'Someone Else']
+    ),
+    new Book(
+      '120218235',
+      'Hello Leipzig',
+      'stellenanzeigen.de',
+      'https://',
+      'Lorem Ipsum',
+      ['Dr. Author', 'Someone Else']
+    )
+  ];
 }
