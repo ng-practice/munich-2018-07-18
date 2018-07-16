c6c73ec2543199ac3325e41e12ccb0075724ee72
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-07-16 13:49:36 +0200

    component: introduce book-card

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [BookCardComponent],
+  exports: [BookCardComponent]
 })
 export class BookModule { }
