f1ba9c76bc19703ea862d4a1435ad2f59e523cde
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-07-18 14:07:43 +0200

    introduce async pipe

--- src/app/book/book-details/book-details.component.ts
+++ src/app/book/book-details/book-details.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ActivatedRoute } from '../../../../node_modules/@angular/router';
-import { switchMap } from '../../../../node_modules/rxjs/operators';
+import { switchMap, takeUntil } from '../../../../node_modules/rxjs/operators';
 import { BookService } from '../lib/book.service';
 import { Book } from '../models/book';
+import {
+  Subscription,
+  Subject,
+  Observable
+} from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'sde-book-details',
@@ -11,14 +16,32 @@ import { Book } from '../models/book';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent implements OnInit {
-  book: Book = {} as Book;
+  // book: Book = {} as Book;
+  book$: Observable<Book>;
+
+  // paramSubscription: Subscription;
+  // destroy$$ = new Subject();
 
   constructor(private route: ActivatedRoute, private books: BookService) {}
 
   ngOnInit() {
-    // this.route.params.subscribe(params => params.isbn)
-    this.route.params
-      .pipe(switchMap(params => this.books.getByIsbn(params.isbn)))
-      .subscribe(book => (this.book = book));
+    this.book$ = this.route.params.pipe(
+      // takeUntil(this.destroy$$),
+      switchMap(params => this.books.getByIsbn(params.isbn))
+    );
+    // .subscribe(
+    //   // next
+    //   book => (this.book = book),
+    //   // err
+    //   () => {},
+    //   // complete
+    //   () => console.log('FINISHED')
+    // );
   }
+
+  // ngOnDestroy(): void {
+  //   this.destroy$$.next();
+  //   this.destroy$$.complete();
+  //   // this.paramSubscription.unsubscribe();
+  // }
 }
